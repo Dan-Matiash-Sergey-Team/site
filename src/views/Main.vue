e1a3d2124496d4d4251f8ad898ad1cfaa92febf9
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2021-01-15 15:49:23 +0300

    cluster

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,11 +2,14 @@
     <div>
 
         <div v-if="showMap  ">
-            <yandex-map :coords="[55.84547, 37.45442]" :use-object-manager="true"
+            <yandex-map :cluster-options="cluster" :clusterOptions="{1:{}}" :coords="[55.84547, 37.45442]"
+                        :use-object-manager="true"
                         @map-was-initialized="initHandler" style="width: 1550px; height: 825px;"
             >
                 <ymap-marker :coords="[Number(marker.data.infoDtp.COORD_W),Number(marker.data.infoDtp.COORD_L)]"
-                             :key="i" :marker-id="i" marker-type="placemark"
+                             :key="i" :marker-id="i" cluster-name="1"
+                             marker-type="placemark"
+
                              v-for="(marker, i) in dtps"
                 >
 
@@ -24,7 +27,15 @@
         data() {
             return {
                 dtps: this.$store.getters.dtps,
-                showMap: false
+                showMap: false,
+                cluster: {
+                    1: {
+                        clusterDisableClickZoom: false,
+                        hasBalloon: false,
+                        clusterLayout: '<div style="color: tomato; font-family: Foros; font-weight: 600;">{{ properties.geoObjects.length }}</div>',
+                        clusterNumbers: [50]
+                    }
+                }
             }
         },
         computed: {
