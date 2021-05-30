32425cd548219ffa4a45ab47df9a8da8aea79b24
Universai <solomatin377@gmail.com>
2021-05-30 13:06:38 +0300

    popup

--- src/views/infgr.vue
+++ src/views/infgr.vue
@@ -1,49 +1,42 @@
 <template>
-  <div>
-
-    <nav class="level has-background-white mb-0" style="height: 50px">
-      <p class="level-item has-icons-left">
-        <router-link to="/map">
-          <i aria-hidden="true" class="link is-info fa fa-home fa-lg"></i>
-        </router-link>
-      </p>
-      <p class="level-item">
-        <strong style="color:black">Карта ДТП по Москве</strong>
-      </p>
-      <p class="level-item has-icons-right">
-        <i aria-hidden="true" class="link is-info fa fa-bar-chart fa-lg"></i>
-      </p>
-    </nav>
-
-    <div class="select">
-      <select id="district" v-model="district">
-        <option :key="opt" v-for="opt in options['district']">
-          {{ opt }}
-        </option>
-      </select>
+  <div class="modal is-active" v-if="showPopup">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <GChart
+          type="LineChart"
+          :data="chartBuild"
+          :options="opts"
+      />
+      <button style="float: right" @click="closePopup">
+        Jopa
+      </button>
     </div>
-
-    <GChart
-        type="LineChart"
-        :data="chartBuild"
-        :options="opts"
-    />
-
   </div>
 </template>
 
 <script>
 import Data from "@/assets/ahegao.json";
 import {GChart} from 'vue-google-charts';
-import Cases from '../assets/Cases.json'
+import Cases from '../assets/Cases.json';
 
 export default {
   name: "infgr",
   components: {
     GChart
   },
-  methods: {},
+  methods: {
+    closePopup: function () {
+      this.$emit('closePopup')
+    }
+  },
+  props: [
+    'showPopup',
+    'districtPopup'
+  ],
   computed: {
+    district: function () {
+      return this.districtPopup
+    },
     countCases: function () {
       let res = 0
       for (let i in this.chartBuild) {
@@ -53,10 +46,16 @@ export default {
     },
     opts: function () {
       return {
-        title: `Общее количество аварий: ${this.countCases}`,
+        title: `Общее количество аварий по району ${this.district}: ${this.countCases}`,
         legend: {position: 'none'},
         hAxis: {
-          textPosition: 'none'
+          textPosition: 'none',
+        },
+        chartArea: {
+          left: 20,
+          top: 20,
+          bottom: 10,
+          right: 20
         }
       }
     },
@@ -65,7 +64,7 @@ export default {
       resdat.push(['Год:Месяц', 'Аварии'])
       for (let y = 2015; y < 2022; y++) {
         for (let m = 1; m < 13; m++) {
-          if(y == 2021 && m > 4) break
+          if (y == 2021 && m > 4) break
           resdat.push([y + ":" + m, this.cases[this.district][y - 2015][m - 1]])
         }
       }
@@ -74,7 +73,6 @@ export default {
   },
   data() {
     return {
-      district: "Южное Тушино",
       dtps: [],
       options: Data,
       cases: Cases['cases']
